6081f4ec36243ca135752c32a7c16dc00e6944ae
Spencer Arnold <sa.variable@gmail.com>
2019-02-02 01:04:47 -0600

    refined logic (especially in resource control) and imported main logic core into middleware task.

--- antiCSRFTest/antiCSRFTest/Startup.cs
+++ antiCSRFTest/antiCSRFTest/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using AntiCSRFTest.Middleware;
 namespace AntiCSRFTest
 {
     public class Startup
@@ -25,10 +25,20 @@ namespace AntiCSRFTest
                 app.UseDeveloperExceptionPage();
             }
 
+            //Catches any Request where method is GET or POST. GET and POST handling are the current scope for this.
+            //Could certainly be extended to other methods down the road if implemented within a web api.
+            //NOTE: UseWhen allows further middleware to be executed
+            //if and only if the next one is invoked. This allows for the charactaristics of an authentication filter that only lets through
+            //Authenticated Requests, if we choose to not invoke _next. UseWhen allows us to create a request branch and rejoin if it passes our
+            //antiCSRF filter.
+            app.UseWhen(context => (context.Request.Method.Equals("GET") || context.Request.Method.Equals("POST")), appBuilder =>
+            {
+                appBuilder.UseMiddleware<AntiCSRFMiddleware>();
+            });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("You passed the antiCSRF filter!");
             });
         }
     }
